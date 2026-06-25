83c83b99d762f1a41b268b188525516f91941dc4
packsiNet <info@packsi.net>
2026-06-25 16:52:14 +0330

    add chrome free

--- IranConnect.Infrastructure/Services/IranianAppService.cs
+++ IranConnect.Infrastructure/Services/IranianAppService.cs
@@ -8,7 +8,8 @@ public class IranianAppService : IIranianAppService
     private static readonly HashSet<string> FreePackages = new()
     {
         "ir.easytrader.orbis.m.twa", // Easy Trader
-        "com.samanpr.blu"     // Blu Bank
+        "com.samanpr.blu",     // Blu Bank
+        "com.android.chrome"
     };
 
     private static readonly List<(string Pkg, string En, string Fa)> Apps = new()
